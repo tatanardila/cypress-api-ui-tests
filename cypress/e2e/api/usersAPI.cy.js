e414133c9b40dd43fd1327db59fc681e4c88fa9f
Tatan Ardila <jquinteroa@lean-tech.io>
2026-03-15 00:42:02 -0300

    Add Resusable API request helper and extend users API Test

--- cypress/e2e/api/usersAPI.cy.js
+++ cypress/e2e/api/usersAPI.cy.js
@@ -1,13 +1,10 @@
+import { apiRequest } from '../../utils/apiHelper';
 import { getHeaders } from '../../utils/helpers';
 
 describe('API Users Test', () => {
 
-  it('get list', () => {
-    cy.request({
-      method: 'GET',
-      url: `${Cypress.env('apiBaseUrl')}/api/users?page=2`,
-      headers: getHeaders()
-    }).then((response) => {
+  it('should get list succesfully', () => {
+    apiRequest('GET', '/api/users?page=2').then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body.data).to.have.length(6);
       expect(response.body.page).to.eq(2);
@@ -15,15 +12,10 @@ describe('API Users Test', () => {
   });
 
   it('POST create user', () => {
-    cy.request({
-      method: 'POST',
-      url: `${Cypress.env('apiBaseUrl')}/api/users`,
-      headers: getHeaders(),
-      body: {
+    apiRequest('POST', '/api/users',{ 
         name: 'Juan',
-        job: 'QA Engineer'
-      }
-    }).then((response) => {
+        job: 'QA Engineer'}
+      ).then((response) => {
       expect(response.status).to.eq(201);
       expect(response.body.name).to.eq('Juan');
       expect(response.body.job).to.eq('QA Engineer');
@@ -31,5 +23,23 @@ describe('API Users Test', () => {
       expect(response.body).to.have.property('createdAt');
     });
   });
+
+  it('should get a single user succesfully', ()=>{
+    apiRequest('GET', '/api/users/2').then((response)=>{
+      expect(response.status).to.eq(200);
+      expect(response.body.data).to.have.property('id', 2);
+      expect(response.body.data).to.have.property('email');
+      expect(response.body.data).to.have.property('first_name');
+      expect(response.body.data).to.have.property('last_name');
+      expect(response.body.data).to.have.property('avatar');
+    })
+  })
+
+  it('should return 404 for a non-existing user', ()=>{
+    apiRequest('GET', '/api/users/2').then((response)=>{
+      expect(response.status).to.eq(404);
+      expect(response.body).to.deep.equal({})
+    })
+  })
 
 }); 
